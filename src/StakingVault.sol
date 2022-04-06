c4b69a7a476776d4c618d7380ee7558eba6040d6
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-04-07 01:39:50 +0200

    Repo setup

---
+++ src/StakingVault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+/**
+ * @title StakingVault
+ * @notice A voting-escrow staking rewards system, built using an EIP-4626 tokenized vault.
+ */
+contract StakingVault {
+
+    constructor(){}
+
+}
